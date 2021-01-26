dae45c0f4ea45d567174f5cc28ab7f7000d10b54
Brandon Makin <bmicmak@gmail.com>
2021-01-25 23:05:42 -0800

    a basic GUI_MultiImagePreviewer is working
    GUI_MultiImagePreviewer allows you to display an arbitrary number of images, cycling through them and showing them one at a time.

--- gui/previewer.py
+++ gui/previewer.py
@@ -24,46 +24,60 @@ def resize_pil_image(image, max_width, max_height):
     if (max_width is not None and max_height is not None): # change the image size to fit in a thumbnail
         max_width = int(root.winfo_fpixels(max_width)) # convert the max_width to device pixels from physical units
         max_height = int(root.winfo_fpixels(max_height)) # convert the max_height to device pixels from physical units
-        print(max_width, max_height)
         image.thumbnail((max_width, max_height), Image.ANTIALIAS)
 
 def example_image(dimensions = None):
     path = r'C:\Users\bmicm\OneDrive\Documents\GitHub\EyeTrackingBlurring\data\first 50 images\input\images\04.jpg'
     return image_from_file(path, dimensions)
 
-class GUI_MultiImagePreviewer(Frame):
-    def __init__(self, parent):
-        Frame.__init__(self, parent)
-        label = Label(self, text="Hello, world")
-        label.pack()
+TYPE_ARRAY = 0
+TYPE_FILEPATH = 1
 
-# class GUI_SingleImage(Frame):
-#     def __init__(self, parent, max_width=None, max_height=None, numpy_image=None, filepath=None):
-#         Frame.__init__(self, parent)
-#         self.label = None
-#         self.set(max_width, max_height, numpy_image, filepath)
-#
-#     def set(self, max_width=None, max_height=None, numpy_image=None, filepath=None):
-#         if (numpy_image is not None):
-#             self.image = image_from_array(numpy_image, max_width, max_height) # save a ref to the image so the garbage collector doesn't delete the image
-#         elif (filepath is not None):
-#             self.image = image_from_file(filepath, max_width, max_height)
-#         else:
-#             raise Exception("No source image has been specified. Please specify either `numpy_image` or `filepath` as an argument")
-#
-#         if (self.label is not None):
-#             self.label.destroy()
-#         self.label = Label(self, image=self.image)
-#         self.label.pack()
+class GUI_MultiImagePreviewer(Frame):
+    def __init__(self, parent, image_sources, data_type, max_width=None, max_height=None):
+        Frame.__init__(self, parent)
+        self.image_sources = image_sources
+        self.image_frame = GUI_SingleImage(root, image_sources[0], data_type, max_width, max_height)
+        self.image_frame.pack()
+        self.data_type = data_type
+        self.index = 0
+        Button(self, text="Next", command=self.next).pack()
+
+    def next(self):
+        self.index = (self.index + 1) % len(self.image_sources)
+        self.image_frame.set(self.image_sources[self.index], self.data_type)
+
+class GUI_SingleImage(Frame):
+    def __init__(self, parent, image_source, data_type, max_width=None, max_height=None):
+        Frame.__init__(self, parent)
+        self.label = None
+        self.max_width = max_width
+        self.max_height = max_height
+        self.set(image_source, data_type)
+
+    def set(self, image_source, data_type):
+        if (data_type == TYPE_ARRAY):
+            self.image = image_from_array(image_source, self.max_width, self.max_height) # save a ref to the image so the garbage collector doesn't delete the image
+        elif (data_type == TYPE_FILEPATH):
+            self.image = image_from_file(image_source, self.max_width, self.max_height)
+        else:
+            raise Exception("Please specify the data_type, i.e., the format that image is in. (TYPE_ARRAY, TYPE_FILEPATH, ...)")
+
+        if (self.label is not None):
+            self.label.destroy()
+        self.label = Label(self, image=self.image)
+        self.label.pack()
 
 root = Tk()
 
 path = r'C:\Users\bmicm\OneDrive\Documents\GitHub\EyeTrackingBlurring\data\first 50 images\input\images\04.jpg'
+path2 = r'C:\Users\bmicm\OneDrive\Documents\GitHub\EyeTrackingBlurring\data\first 50 images\input\images\05.jpg'
+path3 = r'C:\Users\bmicm\OneDrive\Documents\GitHub\EyeTrackingBlurring\data\first 50 images\input\images\06.jpg'
 cv2_image = cv2.imread(path)
 
-GUI_MultiImagePreviewer(root)
+GUI_MultiImagePreviewer(root, [path, path2, path3], data_type = TYPE_FILEPATH, max_width="4i", max_height="4i").pack()
 
-# GUI_SingleImage(root, numpy_image=cv2_image, max_width="2i", max_height="2i").pack()
-# GUI_SingleImage(root, filepath=path, max_width="1i", max_height="1i").pack()
+# GUI_SingleImage(root, cv2_image, data_type = TYPE_ARRAY, max_width="2i", max_height="2i").pack()
+# GUI_SingleImage(root, image_source=path, data_type=TYPE_FILEPATH, max_width="1i", max_height="1i").pack()
 # GUI_SingleImage(root).pack()
 root.mainloop()
